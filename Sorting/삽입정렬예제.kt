0aab7050271c0d79f30f21fceac2bd0fa357cf71
seokan-jeong <kd04199@gmail.com>
2022-02-24 00:44:54 +0900

    [Sorting] 삽입정렬예제

---
+++ Sorting/삽입정렬예제.kt
@@ -0,0 +1,16 @@
+fun main() {
+    // 선택 정렬(Selection Sort)
+    var array = arrayListOf<Int>(7, 5, 9, 0, 3, 1, 6, 2, 4, 8)
+
+    for(i in 1 until array.size){
+        for(j in i downTo(1) step(1)){
+            if(array[j] < array[j - 1]){    // 한 칸씩 왼쪽으로 이동
+                array[j] = array[j - 1].also {array[j - 1] = array[j]}
+            }else{
+                break
+            }
+        }
+    }
+
+    print(array)
+}
